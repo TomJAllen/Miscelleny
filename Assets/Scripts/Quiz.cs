b4ef86bcbfc987d1716ada5bba7f93ed2c95b71f
Tom Allen <tomallen_1@hotmail.co.uk>
2023-03-22 11:15:21 +1100

    added simple score system

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -8,7 +8,8 @@ public class Quiz : MonoBehaviour
 {
   [Header("Questions")]
   [SerializeField] TextMeshProUGUI questionText;
-  [SerializeField] QuestionSO question;
+  [SerializeField] List<QuestionSO> questions = new List<QuestionSO>();
+  QuestionSO currentQuestion;
 
   [Header("Answers")]
   [SerializeField] GameObject[] answerButtons;
@@ -20,16 +21,18 @@ public class Quiz : MonoBehaviour
   [SerializeField] Sprite correctAnswerSprite;
 
   [Header("Timer")]
-
   [SerializeField] Image timerImage;
   Timer timer;
+
+  [Header("Scoring")]
+  [SerializeField] TextMeshProUGUI scoreText;
+  ScoreKeeper scoreKeeper;
 
 
   void Start()
     {
       timer = FindObjectOfType<Timer>();
-      // DisplayQuestion();
-      GetNextQuestion();
+      scoreKeeper = FindObjectOfType<ScoreKeeper>();
     }
 
     void Update()
@@ -52,10 +55,27 @@ public class Quiz : MonoBehaviour
 
     void GetNextQuestion()
     {
-      SetButtonState(true);
-      SetDefaultButtonSprite();
-      DisplayQuestion();
+      if(questions.Count > 0)
+      {
+        SetButtonState(true);
+        SetDefaultButtonSprite();
+        GetRandomQuestion();
+        DisplayQuestion();
+        scoreKeeper.IncrementQuestionsSeen();
+      }
     }
+
+    void GetRandomQuestion()
+    {
+      int index = Random.Range(0, questions.Count);
+      currentQuestion = questions[index];
+      if(questions.Contains(currentQuestion))
+      {
+        questions.Remove(currentQuestion);
+      }
+    }
+
+
 
   void SetDefaultButtonSprite()
   {
@@ -74,32 +94,34 @@ public class Quiz : MonoBehaviour
         DisplayAnswer(index);
         SetButtonState(false);
         timer.CancelTimer();
+        scoreText.text = "Score: " + scoreKeeper.CalculateScore() + "%";
     }
 
     void DisplayQuestion()
     {
-      questionText.text = question.GetQuestion();
+      questionText.text = currentQuestion.GetQuestion();
 
       for (int i = 0; i < answerButtons.Length; i++)
       {
         TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-        buttonText.text = question.GetAnswer(i);
+        buttonText.text = currentQuestion.GetAnswer(i);
       }
     }
 
     void DisplayAnswer(int index)
     {
       Image buttonImage;
-      if (index == question.GetCorrectAnswerIndex())
+      if (index == currentQuestion.GetCorrectAnswerIndex())
         {
           questionText.text = "Correct";
           buttonImage = answerButtons[index].GetComponent<Image>();
           buttonImage.sprite = correctAnswerSprite;
+          scoreKeeper.IncrementCorrectAnswers();
         }
       else
         {
-          correctAnswerIndex = question.GetCorrectAnswerIndex();
-          string correctAnswer = question.GetAnswer(correctAnswerIndex);
+          correctAnswerIndex = currentQuestion.GetCorrectAnswerIndex();
+          string correctAnswer = currentQuestion.GetAnswer(correctAnswerIndex);
           questionText.text = $"Incorrect, the correct answer is {correctAnswer}";
           buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
           buttonImage.sprite = correctAnswerSprite;
